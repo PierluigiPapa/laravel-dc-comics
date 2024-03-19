0480dcaea353843d46b0020df5e73670f24695b2
PierluigiPapa <pierluigipapa9@gmail.com>
2024-03-19 16:30:38 +0100

    fix: adjust font tag a on welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
 <h1 class="text-center">BENVENUTI SU DC COMICS</h1>
 <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
-    <a href="{{ route('comicsData.index') }}" class="btn btn-primary fw-bolder border-1 border-dark">
+    <a href="{{ route('comicsData.index') }}" class="btn btn-primary fw-bolder fs-1 border-1 border-dark">
         VISUALIZZA FUMETTI
     </a>
 </div>
